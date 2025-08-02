a7eb4588b929f117cb21f3327f10c05480cfb76f
pugal1510 <108538519+pugal1510@users.noreply.github.com>
2025-08-02 11:46:00 +0530

    Structure_adding

---
+++ POC_project/POC_project/src/main/java/com/example/POC_project/PocProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.POC_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocProjectApplication.class, args);
+	}
+
+}
